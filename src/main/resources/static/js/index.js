f4d92788023b517fdc45fc676d02988fab573146
John <cwj19880816@hotmail.com>
2019-06-25 16:11:37 +0800

    first commit

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
     $(".person_option").hide();
-    $("#div_top_user_msg").hover(function () {
-        $(".person_option").slideDown("normal");
+    $("#div_top_user").hover(function () {
+        $(".person_option").show();
     },function () {
-        $(".person_option").slideUp("normal");
+        $(".person_option").hide();
+    });
+    $(".person_option li").hover(function () {
+        $(this).removeClass("divwhite").addClass("divblue");
+    },function () {
+        $(this).removeClass("divblue").addClass("divwhite");
     });
 });
